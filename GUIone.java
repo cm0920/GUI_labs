a6976fae23f7242eaaa01af93cf6b35b63cfc8a3
cm0920 <75263530+cm0920@users.noreply.github.com>
2022-01-07 11:26:01 -0600

    Update GUIone.java

--- GUIone.java
+++ GUIone.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 
 public class GUIone extends JFrame{
     public static void main(String[] args){
-        int one = JOptionPane.showConfirmDialog(null, "No Message", "Message Box Example", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
-        if(one == JOptionPane.OK_OPTION){
+        JOptionPane.showConfirmDialog(null, "No Message", "Message Box Example", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+        /*if(one == JOptionPane.OK_OPTION){
             int two = JOptionPane.showConfirmDialog(null, "WATCH OUT!!!", "Message Box Example", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE);
             if(two == JOptionPane.YES_OPTION){
                 int three = JOptionPane.showConfirmDialog(null, "You are wrong.", "Message Box Example", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
@@ -18,5 +18,12 @@ public class GUIone extends JFrame{
         else {
 
         }
+
+         */
+        JOptionPane.showConfirmDialog(null, "WATCH OUT!!!", "Message Box Example", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE);
+        JOptionPane.showConfirmDialog(null, "You are wrong.", "Message Box Example", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
     }
+
+
 }
+
